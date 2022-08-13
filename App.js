a5f75818b754c9d5544482fb53d6e12437c5f1c7
samvillasmith <samuel.villa.smith@hotmail.com>
2022-08-12 21:41:41 -0500

    added  photos and description to initial rental

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
+        <View style={{display: "flex", flexDirection: "row"}}>
+          <Image 
+          source={{uri:"https://picsum.photos/200/300" }} 
+          style={styles.postImage}></Image>
+          <View style={{ justifyContent: "space-around", paddingVertical: 10}}>
+            <View>
+                <Text>Title for Rent</Text>
+                <Text>New York</Text>
+            </View>
+            <Text>$100 per day</Text>
+          </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +24,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fec85c',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "#fec85c10"
   },
+  postImage: {
+    height: 100, 
+      width: 100, borderRadius: 30, 
+      marginLeft: 10, 
+      marginRight: 20,
+      marginVertical: 10
+},
 });
